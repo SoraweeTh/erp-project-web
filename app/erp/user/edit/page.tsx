e2daef319edbe4cd7265a387e9620da6868585a7
SoraweeTh <soraweethanachai@gmail.com>
2025-05-17 18:28:35 +0700

    user management page and modal

---
+++ app/erp/user/edit/page.tsx
@@ -0,0 +1,119 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Config } from "@/app/Config"
+import Swal from "sweetalert2"
+import { useRouter } from "next/navigation"
+
+export default function EditProfile() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const router = useRouter();
+
+    useEffect(() => {
+        fetchUserData();
+    }, []);
+
+    const fetchUserData = async () => {
+        try {
+            const token = localStorage.getItem(Config.TokenKey);
+            const response = await axios.get(`${Config.apiUrl}/api/users/admin-info`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.status === 200) {
+                setUsername(response.data.username);
+                setEmail(response.data.email);
+            }
+        } catch (err) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Unable to get user data'
+            });
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        try {
+            if (password !== confirmPassword) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Password is not matched'
+                });
+                return;
+            }
+
+            const token = localStorage.getItem(Config.TokenKey);
+            console.log('token - ' + token);
+            const url = `${Config.apiUrl}/api/users/admin-edit-profile`;
+            const payload = {
+                username: username,
+                email: email,
+                password: password
+            }
+            const headers = {
+                'Authorization': `Bearer ${token}`
+            }
+            console.log('header - ' + headers);
+            const response = await axios.post(url, payload, {headers});
+
+            if (response.status === 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'Edit profile successful!'
+                });
+                router.push('/erp/dashboard');
+            }
+        } catch (err) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Unable to edit profile'
+            });
+        }
+    }
+
+    return (
+        <div>
+            <h1 className="login-title">Edit User Profile</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="form-label">Username</label>
+                    <input type="text" className="form-input" value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Email</label>
+                    <input type="email" className="form-input" value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-input"
+                        onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label className="form-label">Confirm Password</label>
+                    <input type="password" className="form-input"
+                        onChange={(e) => setConfirmPassword(e.target.value)} />
+                </div>
+                <div className="form-group flex items-center">
+                    <button type="submit" className="button">
+                        <i className="fas fa-save mr-2"></i>
+                        Save
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ app/erp/user/page.tsx
@@ -0,0 +1,207 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import Swal from "sweetalert2"
+import { Config } from "@/app/Config"
+import Modal from "../components/Modal"
+import axios from "axios"
+
+interface User {
+    id: number,
+    email: string,
+    username: string
+}
+
+export default function User() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [showModal, setShowModal] = useState(false);
+    const [userId, setUserId] = useState<number>();
+    const [editingUser, setEditingUser] = useState<User | null>(null);
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get(`${Config.apiUrl}/api/users`);
+            if (response.status == 200) {
+                setUsers(response.data);
+            }
+        } catch (err: any) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.message
+            })
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const url = editingUser 
+            ? `${Config.apiUrl}/api/users/admin-update/${editingUser.id}` 
+            : `${Config.apiUrl}/api/users/admin-create`;
+            const payload = {
+                id: editingUser?.id || null,
+                email: email,
+                username: username,
+                password: password || ''
+            }
+            const headers = {
+                'Authorization': 'Bearer ' + localStorage.getItem(Config.TokenKey)
+            }
+
+            const response = await axios.post(url, payload, {headers});
+            if (response.status === 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: `User ${editingUser ? 'updated' : 'created'} successfully`,
+                    timer: 1000
+                })
+                setShowModal(false);
+                setEditingUser(null);
+                setEmail('');
+                setUsername('');
+                setPassword('');
+                fetchUsers();
+            }
+        } catch (err: any) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.message
+            })
+        }
+    }
+
+    const handleDelete = async (user: User) => {
+        try {
+            const result = await Swal.fire({
+                icon: 'warning',
+                title: 'ARE YOU SURE !',
+                text: `Do you want to delete ${user.username} ?`,
+                showCancelButton: true,
+                showConfirmButton: true
+            })
+
+            if (result.isConfirmed) {
+                const headers = {
+                    'Authorization': 'Bearer ' + localStorage.getItem(Config.TokenKey)
+                }
+                const response = await axios.delete(`${Config.apiUrl}/api/users/admin-delete/${user.id}`, {headers});
+                if (response.status === 200) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success',
+                        text: 'User deleted successfully',
+                        timer: 1000
+                    })
+                    fetchUsers();
+                }
+            }
+        } catch (err: any) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.message
+            })
+        }
+    }
+
+    const handleEdit = (user: User) => {
+        console.log('user id: ' + user.id);
+        setEditingUser(user);
+        setEmail(user.email);
+        setUsername(user.username);
+        setPassword('');
+        setShowModal(true);
+    }
+    
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-5">User Management</h1>
+            <div className="flex justify-between items-center mb-6">
+                <button className="button-add" 
+                    onClick={() => {
+                        setEditingUser(null)
+                        setEmail('')
+                        setUsername('')
+                        setPassword('')
+                        setShowModal(true)
+                    }}>
+                    <i className="fas fa-plus mr-2"></i>
+                    Add User
+                </button>
+            </div>
+            <div className="bg-white shadow-md rounded-lg overflow-hidden">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Email</th>
+                            <th className="w-[120px]">Username</th>
+                            <th className="text-right" style={{width: '100px'}}>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(user => (
+                            <tr key={user.id}>
+                                <td>{user.email}</td>
+                                <td>{user.username}</td>
+                                <td className="text-right">
+                                    <button className="table-action-btn table-edit-btn mr-2"
+                                        onClick={() => handleEdit(user)}
+                                    >
+                                        <i className="fas fa-edit"></i>
+                                    </button>
+                                    <button className="table-action-btn table-delete-btn mr-2"
+                                        onClick={() => handleDelete(user)}
+                                    >
+                                        <i className="fas fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            {showModal && (
+                <Modal id="user-modal" title="user info" onClose={() => setShowModal(false)} size="md">
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block mb-2">Email</label>
+                            <input type="email" className="form-input" value={email}
+                                onChange={e => setEmail(e.target.value)} required />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2">Username</label>
+                            <input type="text" className="form-input" value={username}
+                                onChange={e => setUsername(e.target.value)} required />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2">Password</label>
+                            <input type="password" className="form-input"
+                                onChange={e => setPassword(e.target.value)} />
+                        </div>
+                        <div className="flex justify-end gap-2">
+                            <button type="button" onClick={() => setShowModal(false)}
+                                className="modal-btn modal-btn-cancel">
+                                <i className="fas fa-times mr-2"></i>
+                                Cancel
+                            </button>
+                            <button type="submit" className="modal-btn modal-btn-submit">
+                                <i className="fas fa-check mr-2"></i>
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </Modal>
+            )}
+        </div>
+    );
+}
